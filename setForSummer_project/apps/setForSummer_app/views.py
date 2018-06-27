06de09a09b57da599a02c0988125499ae1a5c7b4
William Enright <williamenright@Spaghetti.local>
2018-06-26 18:03:31 -0700

    resolved ajax issues and attained desired data passthrough methods

--- setForSummer_project/apps/setForSummer_app/views.py
+++ setForSummer_project/apps/setForSummer_app/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render, redirect, HttpResponse
-
+from .models import *
 
 def setHome(request):
-    # return HttpResponse('you made it home')
     return render(request, 'setForSummer_app/index.html')
 
 def food(request):
+
     return render(request, 'setForSummer_app/food.html')
 
 def activities(request):
     return render(request, 'setForSummer_app/activites.html')
 
-def map_html(request,id):
+def map_id(request,id):
     map = Location.objects.get(id=id)
     return render(request,'setForSummer_app/map.html',{
         'lat':map.lat,
-        'lng':map.lon,
+        'lon':map.lon,
     })
-
-# Create your views here.
