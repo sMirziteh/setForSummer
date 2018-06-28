beaf018161104d5e7a08f99e173a0598c0c78e46
shelby mirziteh <shelby.mirziteh@gmail.com>
2018-06-27 18:54:22 -0700

    user database creation

--- setForSummer_project/apps/setForSummer_app/models.py
+++ setForSummer_project/apps/setForSummer_app/models.py
@@ -1,6 +1,23 @@
 from __future__ import unicode_literals
 from django.db import models
 
+class User(models.Model):
+    user_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    phone = models.CharField(max_length=10)
+    zipcode = models.CharField(max_length=5)
+    newsletter = models.BooleanField(default=False)
+    food = models.BooleanField(default=False)
+    activities = models.BooleanField(default=False)
+    learning = models.BooleanField(default=False)
+    relationship = models.CharField(max_length = 255)
+
+    def __repr__(self):
+        return f"<<Users: {self.user_name} {self.email} {self.phone} {self.zipcode} {self.newsletter} {self.food} {self.activities} {self.learning} {self.relationship}"
+
+
+
+
 class Location(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
